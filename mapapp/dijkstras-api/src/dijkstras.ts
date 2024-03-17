62789fc809724e2c112119fb3d8c6bb4ba4a6122
aku7703 <31360520+aku7703@users.noreply.github.com>
2024-03-17 10:35:06 -0400

    Added comments for changes we need to the algo

--- mapapp/dijkstras-api/src/dijkstras.ts
+++ mapapp/dijkstras-api/src/dijkstras.ts
@@ -3,6 +3,21 @@ interface Neighbors {
 
 }
 
+/*
+Note: The dijkstra function is a modified version of the algorithm found at https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+
+We need to now modify the algorithm depending whether a user wants to take the tunnel or wants the fastest route. 
+
+Additionally, whenever we want to update the data points, we need to use the commands "npm run build"
+and then "npm run seed" to send the data to the database.
+
+I also want to see if we can hide my MongoDB password from the public.
+
+-Aniket
+*/
+
+
+
 export default function dijkstra(graph: Record<string, Neighbors>, start: string, end: string): string[] {
     const distances: Record<string, number> = {};
     const previous: Record<string, string | null> = {};
